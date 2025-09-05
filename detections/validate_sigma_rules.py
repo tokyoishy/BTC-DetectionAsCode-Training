b4b0d8ea89cd5a2f499777e52f9205b7e0400136
Ismaeel <71803648+tokyoishy@users.noreply.github.com>
2025-09-05 14:40:00 +0000

    add validation

---
+++ detections/validate_sigma_rules.py
@@ -0,0 +1,28 @@
+import yaml
+import json
+import jsonschema
+import os
+
+# Load schema
+with open('sigma.schema.json') as f:
+    schema = json.load(f)
+
+# Get list of filenames
+filenames = [f for f in os.listdir('.') if f.endswith('.yml') or f.endswith('.yaml')]
+list_of_rules = []
+
+# Load and validate a YAML rule
+for filename in filenames:
+    with open(filename) as f:
+        rule = yaml.safe_load(f)
+        list_of_rules.append(rule)
+
+validator = jsonschema.Draft7Validator(schema, format_checker=jsonschema.FormatChecker())
+
+for rule, filename in zip(list_of_rules, filenames):
+    errors = list(validator.iter_errors(rule))
+    if not errors:
+        print(f"VALID: '{filename}'")
+    else:
+        for error in errors:
+            print(f"INVALID: '{filename}' - {error.message}")
